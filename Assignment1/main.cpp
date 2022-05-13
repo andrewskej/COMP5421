ae44f1cc532cde2b4a6e041419fdefb0994ac958
Andrew Yousik Jeon <andrewskej@gmail.com>
2022-05-12 23:18:55 -0400

    Initial Commit

---
+++ Assignment1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assignment1
+//
+//  Created by Yousik Jeon on 2022-05-12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
